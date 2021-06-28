059c163f1dd1cdc0d4a36879d39c1cc8adb012e2
Luis Jorge <gevolutionxalapa@gmail.com>
2021-06-28 14:04:26 -0500

    Listas y mensaje condicional de citas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, {Fragment, useState} from 'react'
 import Formulario from './components/Formulario'
+import Cita from './components/Cita'
+
 const App = () => {
   // Arreglo de citas
   const [citas, anexarCitas] = useState([])
   // Funcion que tome las citas actuales y tome la nueva
   const crearCita = cita =>{
-    console.log("Este es el elmento del formulario en padre", cita );
     anexarCitas([...citas, cita])
   }
+  // Funcion que elimina una cita por su id
+  const eliminarCita = id =>{
+    const nuevasCitas = citas.filter(cita=> cita.id !== id)
+    anexarCitas(nuevasCitas)    
+  }
+  // Mensaje condicional
+  const titulo = citas.length === 0 
+    ? 'No existen citas'
+    : 'Administra tus citas' 
+
   return (
     <Fragment>
       <h1>Administrador de pacientes</h1>
@@ -15,9 +26,21 @@ const App = () => {
         <div className="row">
           <div className="one-half column">
             <Formulario
-            crearCita={crearCita}/>
+            crearCita={crearCita}
+            />
           </div>
-          <div className="one-half column">2</div>
+          <div className="one-half column">
+            <h2>{titulo}</h2>
+            {
+              citas.map(cita =>(
+                <Cita 
+                  key={cita.id}
+                  cita={cita}
+                  eliminarCita = {eliminarCita}
+                />
+              ))
+            }
+          </div>
         </div>
       </div>
     </Fragment>

278e44fdb77460ca037923b510a0e227df1f333a
Luis Jorge <gevolutionxalapa@gmail.com>
2021-06-28 09:53:34 -0500

    Validacion de formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,17 +1,54 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
 
 const Formulario = () => {
+    const [cita, setCita] = useState({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: '',
+    })
+    const [error, setError] = useState(false)
+    // Funcion que se ejecuta cada vez que se escribe en un input 
+    const acutalizarState = e =>{
+        setCita({
+            ...cita,
+            [e.target.name]: e.target.value
+        })
+    }
+    // Extraccion de propietarios
+    const {mascota, propietario, fecha, hora, sintomas} = cita
+    // Cuando el usuario preciona agregar citas
+    const submitCita = e =>{
+        e.preventDefault();
+        // Validar
+        if(mascota.trim() === '' || propietario.trim() === '' || fecha.trim() === '' || hora.trim() === '' || sintomas.trim() === ''
+        ){
+            setError(true)
+            return 
+        }
+        // Asignar un id
+        // Crear la cita
+        // Reinciar el form 
+    }
     return (
         <Fragment>
             <h1>Crear Cita</h1>
-
-            <form action="">
+            {
+                error 
+                ? <p className="alerta-error">Todos los campos son obligatorios</p>
+                : null
+            }
+            <form 
+            onSubmit={submitCita}>
                 <label>Nombre Mascota</label>
                 <input 
                 type="text"
                 name="mascota"
                 className="u-full-width"
                 placeholder="Nombre Mascota"
+                onChange={acutalizarState}
+                value={mascota}
                 />
                 <label>Nombre Dueño Mascota</label>
                 <input 
@@ -19,18 +56,24 @@ const Formulario = () => {
                 name="propietario"
                 className="u-full-width"
                 placeholder="Nombre dueño Mascota"
+                onChange={acutalizarState}
+                value={propietario}
                 />
                 <label>Fecha</label>
                 <input 
                 type="date"
                 name="fecha"
                 className="u-full-width"
+                onChange={acutalizarState}
+                value={fecha}
                 />
                 <label>Hora</label>
                 <input 
                 type="time"
                 name="hora"
                 className="u-full-width"
+                onChange={acutalizarState}
+                value={hora}
                 />
                 <label>Sintoma</label>
                 <textarea 
@@ -38,7 +81,9 @@ const Formulario = () => {
                     id="" 
                     cols="30" 
                     rows="10"
-                    className="u-full-width">
+                    className="u-full-width"
+                    onChange={acutalizarState}
+                    value={sintomas}>
                 </textarea>
                 <button 
                 type="submit"
